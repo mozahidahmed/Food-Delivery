54b60172c4c38c85f9d9c40ac29ffceaecfb23f6
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-21 22:12:35 +0600

    responsive

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,42 +2,39 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-black text-white'>
-            <div className="grid  lg:grid-cols-4 sm:grid-cols-1 px-6 py-6">
-                <div className="mt-6">
-                    <h1 className='text-3xl font-bold'>PANDAMI</h1>
-                </div>
-                <div className="mt-6">
-                  <h2 className='font-bold text-2xl mb-2'>Company:</h2>
-                  <p>About us</p>
-                  <p>Team</p>
-                  <p>Careers</p>
-                  <p>Campaigns</p>
-                  <p>Join as a Foodstar</p>
-                  <p>Join as a Foodstar</p>
-                </div>
-                <div className="mt-6">
-                  <h2 className='font-bold text-2xl mb-2'>Legal:</h2>
-                  <p>About us</p>
-                  <p>Team</p>
-                  <p>Careers</p>
-                  <p>Campaigns</p>
-                  <p>Join as a Foodstar</p>
-                  <p>Join as a Foodstar</p>
-                </div>
-                <div className="mt-6">
-                  <h2 className='font-bold text-2xl mb-2'>Contact:</h2>
-                  <p>mozahidwebmin11d1-@gmail.com</p>
-                  <p>+8801614853207</p>
-                  
-                </div>
-                
-                
-            </div>
-           <div className="grid justify-center">
-             <img src="https://khanidani.com/assets/images/sslcomm.png" alt="" />
-           </div>
+      <div className="bg-black text-white">
+        <div className="grid  lg:grid-cols-4  md:grid-cols-2 sm:grid-cols-1 px-6 py-6">
+          <div className="mt-6">
+            <h1 className="text-3xl font-bold">PANDAMI</h1>
+          </div>
+          <div className="mt-6">
+            <h2 className="font-bold text-2xl mb-2">Company:</h2>
+            <p>About us</p>
+            <p>Team</p>
+            <p>Careers</p>
+            <p>Campaigns</p>
+            <p>Join as a Foodstar</p>
+            <p>Join as a Foodstar</p>
+          </div>
+          <div className="mt-6">
+            <h2 className="font-bold text-2xl mb-2">Legal:</h2>
+            <p>About us</p>
+            <p>Team</p>
+            <p>Careers</p>
+            <p>Campaigns</p>
+            <p>Join as a Foodstar</p>
+            <p>Join as a Foodstar</p>
+          </div>
+          <div className="mt-6">
+            <h2 className="font-bold text-2xl mb-2">Contact:</h2>
+            <p>mozahidwebmin11d1-@gmail.com</p>
+            <p>+8801614853207</p>
+          </div>
         </div>
+        <div className="grid justify-center">
+          <img src="https://khanidani.com/assets/images/sslcomm.png" alt="" />
+        </div>
+      </div>
     );
 };
 
