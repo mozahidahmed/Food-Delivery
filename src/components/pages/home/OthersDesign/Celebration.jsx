0383bfb4bc2dcf4df9d827dcfc1669016e942e49
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-27 17:33:50 +0600

    desiogn

--- src/components/pages/home/OthersDesign/Celebration.jsx
+++ src/components/pages/home/OthersDesign/Celebration.jsx
@@ -8,54 +8,185 @@ const Celebration = () => {
         <p className="">
           We are always ready to make good food for celebration
         </p>
-        <div className="grid lg:grid-cols-4 md:grid-cols-3  gap-1 py-6">
-          <div className="celebration_wrapper">
-            <img
-              src="	https://duruthemes.com/demo/html/florya/img/services/3.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="	https://duruthemes.com/demo/html/florya/img/services/1.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="	https://duruthemes.com/demo/html/florya/img/services/5.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="https://duruthemes.com/demo/html/florya/img/services/6.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="	https://duruthemes.com/demo/html/florya/img/services/2.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="	https://duruthemes.com/demo/html/florya/img/services/4.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="https://duruthemes.com/demo/html/florya/img/services/6.jpg"
-              alt=""
-            />
-          </div>
-          <div className="celebration_wrapper">
-            <img
-              src="	https://duruthemes.com/demo/html/florya/img/services/2.jpg"
-              alt=""
-            />
+
+        <div class="flex min-h-screen items-center justify-center bg-neutral-300">
+          <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="https://duruthemes.com/demo/html/florya/img/services/3.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Aperitives
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="	https://duruthemes.com/demo/html/florya/img/services/1.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Drinks
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="https://duruthemes.com/demo/html/florya/img/services/5.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Desserts
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="https://duruthemes.com/demo/html/florya/img/services/6.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Breakfast
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="	https://duruthemes.com/demo/html/florya/img/services/4.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Breakfast
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="https://duruthemes.com/demo/html/florya/img/services/6.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Breakfast
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="	https://duruthemes.com/demo/html/florya/img/services/1.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Drinks
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
+            <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div class="h-96 w-72">
+                <img
+                  class="h-full w-full object-cover transition-transform duration-500  group-hover:scale-125"
+                  src="https://duruthemes.com/demo/html/florya/img/services/5.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">
+                  Desserts
+                </h1>
+                <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              More Details
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/home/OthersDesign/Ldb.jsx
+++ src/components/pages/home/OthersDesign/Ldb.jsx
@@ -1,36 +1,100 @@
-import React from 'react';
+import React from "react";
 
 const Ldb = () => {
-    return (
-      <div>
-        <div className="grid lg:grid-cols-2 md:grid-cols-2">
-          <div className="Ldb_wrapper">
-            <img
-              src="https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_4.jpg"
-              alt=""
-            />
+  return (
+    <div>
+      <div class="flex min-h-screen items-center justify-center bg-neutral-300">
+        <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+          <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+              <img
+                class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_4.jpg"
+                alt=""
+              />
+            </div>
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 class="font-dmserif text-3xl font-bold text-white">
+                Aperitives
+              </h1>
+              <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                dolore adipisci placeat.
+              </p>
+              <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+                See Food
+              </button>
+            </div>
           </div>
-          <div className="Ldb_wrapper">
-            <img
-              src="https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_10.jpg"
-              alt=""
-            />{" "}
+          <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+              <img
+                class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_10.jpg"
+                alt=""
+              />
+            </div>
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 class="font-dmserif text-3xl font-bold text-white">Drinks</h1>
+              <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                dolore adipisci placeat.
+              </p>
+              <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+                See Food
+              </button>
+            </div>
           </div>
-          <div className="Ldb_wrapper">
-            <img
-              src="https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_7.jpg"
-              alt=""
-            />
+          <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+              <img
+                class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_7.jpg"
+                alt=""
+              />
+            </div>
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 class="font-dmserif text-3xl font-bold text-white">
+                Desserts
+              </h1>
+              <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                dolore adipisci placeat.
+              </p>
+              <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+                See Food
+              </button>
+            </div>
           </div>
-          <div className="Ldb_wrapper">
-            <img
-              src="	https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_9.jpg"
-              alt=""
-            />
+          <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+              <img
+                class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src="	https://wossthemes.com/jack/wp-content/uploads/2018/01/blog_image_9.jpg"
+                alt=""
+              />
+            </div>
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 class="font-dmserif text-3xl font-bold text-white">
+                Breakfast
+              </h1>
+              <p class="mb-3 text-lg  text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                dolore adipisci placeat.
+              </p>
+              <button class="rounded-full bg-red-500 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+                See Food
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Ldb;
