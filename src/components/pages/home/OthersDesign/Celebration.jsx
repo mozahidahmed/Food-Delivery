4750b82a206f32b954443837436ef94335f11b32
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-27 17:35:47 +0600

    design

--- src/components/pages/home/OthersDesign/Celebration.jsx
+++ src/components/pages/home/OthersDesign/Celebration.jsx
@@ -10,7 +10,7 @@ const Celebration = () => {
         </p>
 
         <div class="flex min-h-screen items-center justify-center bg-neutral-300">
-          <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+          <div class="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-4">
             <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
               <div class="h-96 w-72">
                 <img
--- src/components/pages/home/OthersDesign/Ldb.jsx
+++ src/components/pages/home/OthersDesign/Ldb.jsx
@@ -4,7 +4,7 @@ const Ldb = () => {
   return (
     <div>
       <div class="flex min-h-screen items-center justify-center bg-neutral-300">
-        <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+        <div class="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-4">
           <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
             <div class="h-96 w-72">
               <img
