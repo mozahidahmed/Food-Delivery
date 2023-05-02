1b1871bf7726c9e76857b1d06bd4339ed27f762c
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-05-02 11:36:55 +0600

    render host link

--- src/features/api/apiSlice.jsx
+++ src/features/api/apiSlice.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "RestaurantsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    baseUrl: "https://pandami.onrender.com/",
   }),
   tagTypes: [
     "restaurants",
